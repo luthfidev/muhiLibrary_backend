d5354ff17c9a9c5ddb43f11b353148619b689410
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-10 06:43:40 +0700

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,12 +2,17 @@ require('dotenv').config()
 const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env
 const mysql = require('mysql')
 
-const options = {
+const connection = mysql.createConnection({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASS,
     database: DB_NAME
-}
-const conn = mysql.createConnection(options)
+})
+connection.connect(function(error) {
+    if (error) {
+      return console.log('DB not connect')
+    }
+    console.log('DB is connect')
+})
 
-module.exports = conn
+module.exports = connection
