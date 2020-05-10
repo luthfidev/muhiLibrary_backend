949512110b65f1d6a33ce7dc31da22448d9bec2f
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-10 09:03:56 +0700

    Create validator.js

---
+++ src/helper/validator.js
@@ -0,0 +1,15 @@
+const { check } = require('express-validator')
+
+exports.createBook = [
+    check('title').not().isEmpty().withMessage('Title is required')
+                  .isAlphanumeric().withMessage('Title must be alphanumeric')
+                  .isLength({max: 10}).withMessage('Max character 10'),
+    
+    check('genre_id').not().isEmpty().withMessage('Genre id is required')
+                     .isNumeric().withMessage('genre_id must be numeric')
+                     .isLength({max: 1}).withMessage('Max Number 1'),
+    
+    check('author_id').not().isEmpty().withMessage('Author id is required')
+                      .isNumeric().withMessage('author_id must be numeric')
+                      .isLength({max: 1}).withMessage('Max Number 1')
+]
