1ab5b9ce3404bb99d7ad711bffb07ed306eac41f
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-11 18:05:33 +0700

    create user detail

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 const usersController = require('../controllers/users')
+const validator = require('../helper/validator')
 const verify = require('../helper/verifyToken')
 
 router.get('/', verify, usersController.getAllUsers)
 router.post('/', usersController.createUser)
+router.post('/biodata', verify, validator.createUserDetail, usersController.createUserDetail)
 router.post('/login', usersController.loginUser)
 
 module.exports = router
