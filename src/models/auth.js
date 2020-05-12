496858550ecc6530822eed96ae3bcb047b80dc21
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-13 04:01:58 +0700

    Update auth.js

--- src/models/auth.js
+++ src/models/auth.js
@@ -3,7 +3,9 @@ const db = require('../utils/db')
 module.exports = {
 
     getAuthCondition: (data) => {
-        const sql = 'SELECT * FROM users WHERE ?'
+        const sql = `SELECT users.id, users.email, users.password, roles.name as nameRole, user_details.name as nameUser FROM 
+                    users JOIN roles ON roles.id = users.role_id 
+                         LEFT JOIN user_details ON user_details.user_id = users.id WHERE ?`
         return new Promise((resolve, reject) => {
             db.query(sql, data, (error, results) => {
                 if (error) {
