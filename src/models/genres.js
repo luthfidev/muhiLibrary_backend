4f1bbaf0f77f6e2f5931d377e05dd7b7463ba14b
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-12 11:58:38 +0700

    Create genres.js

---
+++ src/models/genres.js
@@ -0,0 +1,65 @@
+const db = require('../utils/db')
+
+module.exports = {
+
+    getAuthorByCondition: (data) => {
+        const sql = 'SELECT * FROM genres WHERE ?'
+        return new Promise((resolve, reject) => {
+            db.query(sql, data, (error, results) => {
+                if (error) {
+                    reject(Error(error))
+                }
+                resolve(results)
+            })
+        })
+    },
+
+    getAllGenres: () => {
+        const sql = 'SELECT * FROM genres'
+        return new Promise((resolve, reject) => {
+            db.query(sql, (error, results) => {
+                if (error) {
+                    reject(Error(error))
+                }
+                resolve(results)
+            })
+        })
+    },
+
+    createGenre: (data) => {
+        const sql = 'INSERT INTO genres SET ?'
+        return new Promise((resolve, reject) => {
+            db.query(sql, data, (error, results) => {
+                if (error) {
+                    reject(Error(error))
+                }
+                resolve(true)
+            })
+        })
+    },
+
+    updateGenre: (data) => {
+        const sql = 'UPDATE authors SET ? WHERE ?'
+        return new Promise((resolve, reject) => {
+            db.query(sql, data, (error, results) => {
+                if (error) {
+                    reject(Error(error))
+                }
+                resolve(results.affectedRows)
+            })
+        })
+    },
+
+    deleteGenre: (data) => {
+        const sql = 'DELETE FROM genres WHERE ?'
+        return new Promise((resolve, reject) => {
+            db.query(sql, data, (error, results) => {
+                if (error) {
+                    reject(Error(error))
+                }
+                resolve(results.affectedRows)
+            })
+        })
+    }
+
+}
