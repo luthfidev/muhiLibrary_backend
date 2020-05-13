7aaa89e7f9a62e275e1c3f9f76e8d2f08de750de
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-14 04:11:11 +0700

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,7 +9,7 @@ const upload = require('../utils/multer')
 router.get('/', verify, checkRole('admin'), usersController.getAllUsers)
 router.post('/', verify, checkRole('admin'), usersController.createUser)
 router.get('/:id', verify, checkRole('admin'), usersController.getDetailUser)
-router.post('/biodata', verify, checkRole('admin' && 'user'), upload.single('picture'), validator.createUserDetail, usersController.createUserDetail)
+router.post('/biodata', verify, upload.single('picture'), validator.createUserDetail, usersController.createUserDetail)
 
 
 module.exports = router
