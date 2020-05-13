d2f231d25aa8b38ce5b82b05d0e281eca1d3da2c
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-13 09:02:42 +0700

    Update books.js

--- src/routes/books.js
+++ src/routes/books.js
@@ -6,10 +6,10 @@ const verify = require('../utils/verifyToken')
 const checkRole = require('../utils/roles')
 const upload = require('../utils/multer')
 
-router.get('/search/:title', bookController.searchBooks)
-router.get('/', verify, checkRole('admin', 'user'), bookController.getAllBooks)
-router.post('/', upload.single('image'), verify, validator.createBook, bookController.createBook)
-router.patch('/:id', upload.single('image'), validator.updateBook, bookController.updateBook)
-router.delete('/:id', bookController.deleteBook)
+router.get('/search/:title', verify, bookController.searchBooks)
+router.get('/', verify, bookController.getAllBooks)
+router.post('/', verify, checkRole('admin'), upload.single('image'), validator.createBook, bookController.createBook)
+router.patch('/:id', verify, checkRole('admin'), upload.single('image'), validator.updateBook, bookController.updateBook)
+router.delete('/:id', verify, checkRole('admin'), bookController.deleteBook)
 
 module.exports = router
