4dcfddf4e142dd9571f236aa6120cb47fc706c8d
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-14 13:59:04 +0700

    Update transactions.js

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -3,10 +3,12 @@ const checkRole = require('../utils/roles')
 const verify = require('../utils/verifyToken')
 const transactionController = require('../controllers/transactions')
 
+router.use(verify)
+router.use(checkRole('admin'))
 
-router.get('/', verify, checkRole('admin') ,transactionController.getAllTransactions)
-router.post('/', verify, checkRole('admin'), transactionController.createTransaction)
-router.patch('/:id', verify, checkRole('admin'), transactionController.updateTransaction)
-router.delete('/:id', verify, checkRole('admin'), transactionController.deleteTransaction)
+router.get('/', transactionController.getAllTransactions)
+router.post('/', transactionController.createTransaction)
+router.patch('/:id', transactionController.updateTransaction)
+router.delete('/:id', transactionController.deleteTransaction)
 
 module.exports = router
