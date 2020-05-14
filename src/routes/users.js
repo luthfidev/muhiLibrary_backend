25c8f5e31fe4a93351438dfbd2099f79bd27febb
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-14 08:32:22 +0700

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,11 +10,6 @@ router.get('/', verify, checkRole('admin'), usersController.getAllUsers)
 router.post('/', verify, checkRole('admin'), usersController.createUser)
 router.delete('/:id', verify, checkRole('admin'), usersController.deleteUser)
 router.get('/detail/:id', verify, checkRole('admin'), usersController.getDetailUser)
-// router.post('/biodata', verify, upload.single('picture'),  validator.createUserDetail, usersController.createUserDetail)
-router.post('/biodata', verify, upload.single('picture'), validator.createUserDetail, usersController.createUserDetail, function(req, res, next){
-    console.log("file"+req.file+req.files);
-    res.send('Successfully uploaded!');
-  });
-  
+router.post('/biodata', verify, upload.single('picture'),  validator.createUserDetail, usersController.createUserDetail)
 
 module.exports = router
