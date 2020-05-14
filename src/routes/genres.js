18b488d7c9394136de46911485a0e02e31031c31
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-14 13:59:02 +0700

    Update genres.js

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -1,5 +1,10 @@
 const router = require('express').Router()
 const genreController = require('../controllers/genres')
+const verify = require('../utils/verifyToken')
+const checkRole = require('../utils/roles')
+
+router.use(verify)
+router.use(checkRole('admin'))
 
 router.get('/', genreController.getAllGenres)
 router.post('/', genreController.createGenre)
