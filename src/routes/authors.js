95625c6936483eeeef79479dc24a0a3b96ef2688
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-14 13:58:54 +0700

    Update authors.js

--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,12 +1,15 @@
 const router = require('express').Router()
 const validator = require('../utils/validator')
 const authorController = require('../controllers/authors')
+const checkRole = require('../utils/roles')
 const verify = require('../utils/verifyToken')
 
+router.use(verify)
+router.use(checkRole('admin'))
 
-router.get('/', verify, authorController.getAllAuthors)
-router.post('/', verify, validator.author, authorController.createAuthor)
-router.patch('/:id', verify, validator.author, authorController.updateAuthor)
-router.delete('/:id', verify, authorController.deleteAuthor)
+router.get('/', authorController.getAllAuthors)
+router.post('/', validator.author, authorController.createAuthor)
+router.patch('/:id', validator.author, authorController.updateAuthor)
+router.delete('/:id', authorController.deleteAuthor)
 
 module.exports = router
