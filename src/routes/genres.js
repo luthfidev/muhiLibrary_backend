acdc66e3a2345dcf31931cf6fbe312f7f5537bb0
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:19:56 +0700

    Update genres.js

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -5,18 +5,17 @@ const checkRole = require('../utils/roles')
 const cekBiodata = require('../utils/cekBiodata')
 const validator = require('../utils/validator')
 
-
 router.use(verify, cekBiodata)
-      .use(checkRole('admin'))
-      .get('/', 
-            genreController.getAllGenres)
-      .post('/',
-            validator.genre, 
-            genreController.createGenre)
-      .patch('/:id',
-            validator.genre,  
-            genreController.updateGenre)
-      .delete('/:id', 
-            genreController.deleteGenre)
+  .use(checkRole('admin'))
+  .get('/',
+    genreController.getAllGenres)
+  .post('/',
+    validator.genre,
+    genreController.createGenre)
+  .patch('/:id',
+    validator.genre,
+    genreController.updateGenre)
+  .delete('/:id',
+    genreController.deleteGenre)
 
 module.exports = router
