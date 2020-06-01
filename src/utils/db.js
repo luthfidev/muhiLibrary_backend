ccae6e0e7333acefb7dbfc9d06c890f37772c9ac
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:20:05 +0700

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,17 +3,17 @@ const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = process.env
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
-    port: DB_PORT
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  database: DB_NAME,
+  port: DB_PORT
 })
-connection.connect(function(error) {
-    if (error) {
-       return connection.end()
-    }
-   console.log('DB is connect')
+connection.connect(function (error) {
+  if (error) {
+    return connection.end()
+  }
+  console.log('DB is connect')
 })
 
 module.exports = connection
