2491242380e683ad92b8bfd024d8b6b900af4294
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:20:00 +0700

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,22 +6,21 @@ const checkRole = require('../utils/roles')
 const upload = require('../utils/multer')
 
 router.use(verify)
-      .get('/', 
-           checkRole('admin'), 
-           usersController.getAllUsers)
-      .post('/', 
-            checkRole('admin'),
-            validator.signUp, 
-            usersController.createUser)
-      .delete('/:id', 
-            checkRole('admin'), 
-            usersController.deleteUser)
-      .get('/detail/:id', 
-            usersController.getDetailUser)
-      .patch('/biodata', 
-            upload.single('picture'),  
-            validator.updateUserDetail, 
-            usersController.updateUserDetail)
+  .get('/',
+    checkRole('admin'),
+    usersController.getAllUsers)
+  .post('/',
+    checkRole('admin'),
+    validator.signUp,
+    usersController.createUser)
+  .delete('/:id',
+    checkRole('admin'),
+    usersController.deleteUser)
+  .get('/detail/:id',
+    usersController.getDetailUser)
+  .patch('/biodata',
+    upload.single('picture'),
+    validator.updateUserDetail,
+    usersController.updateUserDetail)
 
 module.exports = router
-
