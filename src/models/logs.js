15f8fd3042ff91c02195c07ce2768f7e2e8fc8b8
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:19:26 +0700

    Update logs.js

--- src/models/logs.js
+++ src/models/logs.js
@@ -1,43 +1,43 @@
 const db = require('../utils/db')
 
 module.exports = {
-    
-    getCheckLogin: (email) => {
-        const sql = `SELECT logs.user_email,
+
+  getCheckLogin: (email) => {
+    const sql = `SELECT logs.user_email,
                             logs.type FROM logs
                      WHERE ?`
-        return new Promise((resolve, reject) => {
-            db.query(sql, email, (error, results) => {
-                if (error) {
-                    reject(Error(error))
-                }
-                resolve(results)
-            })
-        })
-    },
+    return new Promise((resolve, reject) => {
+      db.query(sql, email, (error, results) => {
+        if (error) {
+          reject(Error(error))
+        }
+        resolve(results)
+      })
+    })
+  },
 
-    createLogsLogin: (data) => {
-        const sql = `INSERT INTO logs SET ?`
-        return new Promise((resolve, reject) => {
-            db.query(sql, data, (error, results) => {
-                if (error) {
-                    reject(Error(error))
-                }
-                resolve(true)
-            })
-        })
-    },
+  createLogsLogin: (data) => {
+    const sql = 'INSERT INTO logs SET ?'
+    return new Promise((resolve, reject) => {
+      db.query(sql, data, (error, results) => {
+        if (error) {
+          reject(Error(error))
+        }
+        resolve(true)
+      })
+    })
+  },
 
-    deleteLogsLogin: (condition) => {
-        const sql = `DELETE FROM logs WHERE user_email = ? AND type = ?`
-        return new Promise((resolve, reject) => {
-            db.query(sql, [condition.email, condition.type], (error, results) => {
-                if (error) {
-                    reject(Error(error))
-                }
-                resolve(true)
-            })
-        })
-    }
+  deleteLogsLogin: (condition) => {
+    const sql = 'DELETE FROM logs WHERE user_email = ? AND type = ?'
+    return new Promise((resolve, reject) => {
+      db.query(sql, [condition.email, condition.type], (error, results) => {
+        if (error) {
+          reject(Error(error))
+        }
+        resolve(true)
+      })
+    })
+  }
 
 }
