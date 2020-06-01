b8b5d37b11a6ae1b8934872a5a5d81101a586cd2
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:18:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 require('dotenv').config()
-const { APP_URL, APP_PORT } = process.env || 5000
+const { APP_PORT } = process.env || 5000
 const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const helmet = require('helmet')
 
-
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use('/uploads', express.static('uploads'))
-app.use(cors()) 
+app.use(cors())
 app.use(helmet())
 
 app.get('/', (request, response) => {
-    const data = {
-        name: 'Welcome to Muhi Library',
-        version: '1.0.0'
-    }
-    response.send(data)
+  const data = {
+    name: 'Welcome to Muhi Library',
+    version: '1.0.0'
+  }
+  response.send(data)
 })
 
 const auth = require('./src/routes/auth')
@@ -37,11 +36,11 @@ app.use('/bookstatuses', bookstatuses)
 app.use('/transactions', transactions)
 
 app.get('*', (request, response) => {
-    response.status(404).send('Page not found')
+  response.status(404).send('Page not found')
 })
 
 app.listen(APP_PORT, () => {
-    console.log(`Server running`)
+  console.log('Server running')
 })
 
 module.exports = app
