7e02d6d3280a9fc5aab12d0f3295dec39cffd30c
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:19:58 +0700

    Update transactions.js

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -5,30 +5,29 @@ const cekBiodata = require('../utils/cekBiodata')
 const transactionController = require('../controllers/transactions')
 const validator = require('../utils/validator')
 
-
 router.use(verify, cekBiodata)
-      .get('/',
-            checkRole('admin'), 
-            transactionController.getAllTransactions)
-      
-      .get('/userstatus/',
-            transactionController.getTransactionDetailUser)
-      .get('/:id',
-      checkRole('admin'),
-            transactionController.getTransactionDetail) 
-      .post('/',
-            checkRole('admin'), 
-            validator.transaction,
-            transactionController.createTransaction)
-      .post('/user', 
-            validator.userTransaction,
-            transactionController.createUserTransaction)
-      .patch('/:id',
-            checkRole('admin'), 
-            validator.transaction,
-            transactionController.updateTransaction)
-      .delete('/:id', 
-            checkRole('admin'),
-            transactionController.deleteTransaction)
+  .get('/',
+    checkRole('admin'),
+    transactionController.getAllTransactions)
+
+  .get('/userstatus/',
+    transactionController.getTransactionDetailUser)
+  .get('/:id',
+    checkRole('admin'),
+    transactionController.getTransactionDetail)
+  .post('/',
+    checkRole('admin'),
+    validator.transaction,
+    transactionController.createTransaction)
+  .post('/user',
+    validator.userTransaction,
+    transactionController.createUserTransaction)
+  .patch('/:id',
+    checkRole('admin'),
+    validator.transaction,
+    transactionController.updateTransaction)
+  .delete('/:id',
+    checkRole('admin'),
+    transactionController.deleteTransaction)
 
 module.exports = router
