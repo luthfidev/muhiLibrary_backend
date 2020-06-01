ba38ec86b1485d9c28a95b9fd14d0d98553e7ff3
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:20:03 +0700

    Update cekBiodata.js

--- src/utils/cekBiodata.js
+++ src/utils/cekBiodata.js
@@ -1,15 +1,15 @@
 const cekBiodata = (request, response, next) => {
-
-    if (payload.nameUser === null) {
-        const data = {
-            success: false,
-            message: 'Please update your profile'
-        }
-        response.status(400).send(data)
-        return false
-    } else {
-        next();
+  const nameUser = request.payload.nameUser
+  if (nameUser === null) {
+    const data = {
+      success: false,
+      message: 'Please update your profile'
     }
+    response.status(400).send(data)
+    return false
+  } else {
+    next()
+  }
 }
 
 module.exports = cekBiodata
