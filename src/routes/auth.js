435b2547189d43b9e8fe2c613e8579684d3e04fa
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:19:34 +0700

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,13 +4,13 @@ const validator = require('../utils/validator')
 const verify = require('../utils/verifyToken')
 
 router.post('/signin',
-            validator.signIn,  
-            authController.signIn)
-      .post('/signup', 
-            validator.signUp, 
-            authController.signUp)
-      .delete('/logout',
-            verify,  
-            authController.logOut)
+  validator.signIn,
+  authController.signIn)
+  .post('/signup',
+    validator.signUp,
+    authController.signUp)
+  .delete('/logout',
+    verify,
+    authController.logOut)
 
 module.exports = router
