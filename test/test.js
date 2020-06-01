b872e3ed7d493b31c99d373c76eb62eb9a1c7d74
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-01 09:20:17 +0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
-const supertest = require('supertest');
-const chai = require('chai');
-const app = require('../index.js');
+const supertest = require('supertest')
+const chai = require('chai')
+const app = require('../index.js')
 
-global.app = app;
-global.expect = chai.expect;
-global.request = supertest(app);
+const appExpress = app
+var expect = chai.expect
+var request = supertest(appExpress)
 
-describe('Test API Routes', function() {
- /*    // This function will run before every test to clear database
+describe('Test API Routes', function () {
+  /*    // This function will run before every test to clear database
     beforeEach(function(done) {
         app.db.object = {};
         app.db.object.tasks = [{
@@ -23,46 +23,42 @@ describe('Test API Routes', function() {
         done();
     }); */
 
-    // In this test it's expected a task list of two tasks
-    describe('GET /', function() {
-        it('returns a Muhilibray v1', function(done) {
-            request.get('/')
-                .expect(200)
-                .end(function(err, res) {
-                    expect(res.statusCode).to.equal(200);
-                    done(err);
-                });
-        });
+  // In this test it's expected a task list of two tasks
+  describe('GET /', function () {
+    it('returns a Muhilibray v1', function (done) {
+      request.get('/')
+        .expect(200)
+        .end(function (err, res) {
+          expect(res.statusCode).to.equal(200)
+          done(err)
+        })
+    })
 
-        it('returns status 404 not found', function(done) {
-            request.get('/notfound')
-                .expect(404)
-                .end(function(err, res) {
-                    done(err);
-                });
-        });
-    });
+    it('returns status 404 not found', function (done) {
+      request.get('/notfound')
+        .expect(404)
+        .end(function (err, res) {
+          done(err)
+        })
+    })
+  })
 
-    // Testing the save task expecting status 201 of success
-    
-    describe('POST /auth/signin',  function() {
-        it('Login a success', function(done) {
-             request.post('/auth/signin')
-                .set("Connection", "keep alive")
-                .set("Content-Type", "application/json")
-                .send({"email": 'user15@lib.com', password: "user15"})
-                .end(function(err, res) {
-                done(err);
-                process.exit(0)
-            });
-        });
-    });
+  // Testing the save task expecting status 201 of success
 
-  
+  describe('POST /auth/signin', function () {
+    it('Login a success', function (done) {
+      request.post('/auth/signin')
+        .set('Connection', 'keep alive')
+        .set('Content-Type', 'application/json')
+        .send({ email: 'user15@lib.com', password: 'user15' })
+        .end(function (err, res) {
+          done(err)
+          process.exit(0)
+        })
+    })
+  })
 
-
-
-    /* // Here it'll be tested two behaviors when try to find a task by id
+  /* // Here it'll be tested two behaviors when try to find a task by id
     describe('GET /tasks/:id', function() {
         // Testing how to find a task by id
         it('returns a task by id', function(done) {
@@ -76,7 +72,7 @@ describe('Test API Routes', function() {
         });
 
         // Testing the status 404 for task not found
-        
+
     });
 
     // Testing how to update a task expecting status 201 of success
@@ -106,4 +102,4 @@ describe('Test API Routes', function() {
                 });
         });
     }); */
-});
+})
