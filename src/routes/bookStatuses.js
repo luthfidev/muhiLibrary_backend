e96a8c49730d4c5b7a6e004b51f5da04e471a783
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-03 18:10:26 +0700

    Update bookStatuses.js

--- src/routes/bookStatuses.js
+++ src/routes/bookStatuses.js
@@ -3,17 +3,14 @@ const bookStatuses = require('../controllers/bookStatuses')
 const verify = require('../utils/verifyToken')
 const checkRole = require('../utils/roles')
 const cekBiodata = require('../utils/cekBiodata')
-const validator = require('../utils/validator')
 
 router.use(verify, cekBiodata)
   .use(checkRole('admin'))
   .get('/',
     bookStatuses.getAllBookStatuses)
   .post('/',
-    validator.bookstatus,
     bookStatuses.createBookStatus)
   .patch('/:id',
-    validator.bookstatus,
     bookStatuses.updateBookStatus)
   .delete('/:id',
     bookStatuses.deleteBookStatus)
