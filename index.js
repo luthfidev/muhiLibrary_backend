95ce4bcd43780804bd0d61c2ad055a65f4457100
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-03 19:40:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { PORT } = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
