0dd6d93dfd8a629c5348a1152dbafacc1491c82c
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-05 14:19:23 +0700

    Update books.js

--- src/routes/books.js
+++ src/routes/books.js
@@ -4,19 +4,27 @@ const verify = require('../utils/verifyToken')
 const checkRole = require('../utils/roles')
 const cekBiodata = require('../utils/cekBiodata')
 
-router.use(verify, cekBiodata)
-  .get('/',
-    bookController.getAllBooks)
-  .post('/',
-    checkRole('admin'),
-    bookController.createBook)
-  .patch('/:id',
-    checkRole('admin'),
-    bookController.updateBook)
-  .delete('/:id',
-    checkRole('admin'),
-    bookController.deleteBook)
-  .get('/detail/:id',
-    bookController.getDetailBook)
+
+router.get('/',
+        bookController.getAllBooks)
+      .post('/',
+        cekBiodata,
+        verify,
+        checkRole('admin'),
+        bookController.createBook)
+      .patch('/:id',
+        cekBiodata,
+        verify,
+        checkRole('admin'),
+        bookController.updateBook)
+      .delete('/:id',
+        cekBiodata,
+        verify,
+        checkRole('admin'),
+        bookController.deleteBook)
+      .get('/detail/:id',
+        cekBiodata,
+        verify,
+        bookController.getDetailBook)
 
 module.exports = router
