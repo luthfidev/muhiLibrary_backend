12bfc7f29ec7a10947b3d72bc45d3c9ae5a217f4
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-07-28 14:29:10 +0700

    Update muhilibrary.sql

--- db/muhilibrary.sql
+++ db/muhilibrary.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: May 31, 2020 at 01:53 AM
--- Server version: 10.4.12-MariaDB-1:10.4.12+maria~bionic
--- PHP Version: 7.4.5
+-- Waktu pembuatan: 28 Jul 2020 pada 07.27
+-- Versi server: 10.4.12-MariaDB-1:10.4.12+maria~bionic
+-- Versi PHP: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `authors`
+-- Struktur dari tabel `authors`
 --
 
 CREATE TABLE `authors` (
@@ -36,60 +36,41 @@ CREATE TABLE `authors` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `authors`
+-- Dumping data untuk tabel `authors`
 --
 
 INSERT INTO `authors` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Mark Manson', 'Gramedia Widiasarana Indonesia', '2020-05-15 21:55:36', '2020-05-17 13:21:07'),
-(2, 'Hamidah Sulaiman', 'Gramedia', '2020-05-17 22:08:58', '2020-05-17 22:23:46'),
-(3, 'Adrianus Agung W', 'Gramedia', '2020-05-17 22:15:27', '2020-05-17 22:23:55'),
-(4, 'Redaksi Qultum Media', 'Gramedia', '2020-05-17 22:18:11', '2020-05-17 22:23:34'),
-(5, 'Dewi Ria Utari', 'Gramedia', '2020-05-17 22:20:23', '2020-05-17 22:23:38'),
-(6, 'Heru Kurniawan', 'Gramedia', '2020-05-17 22:23:13', '2020-05-17 22:23:41');
+(1, 'DANIEL C. DENNETT', 'DANIEL C. DENNETT', '2020-07-08 23:46:18', '2020-07-12 16:20:38'),
+(5, 'Eko', 'Eko Ivano Winata', '2020-07-09 06:28:19', '2020-07-13 11:27:49'),
+(695, 'Erich Fromm', 'Erich Fromm', '2020-07-09 09:56:18', '2020-07-12 16:24:19'),
+(696, 'Hasna Wijayanti', 'Hasna Wijayanti', '2020-07-09 09:56:48', '2020-07-12 16:23:27'),
+(700, 'Jeriko', 'Jeriko', '2020-07-12 10:09:33', NULL),
+(701, 'Carol S. Dweck, Ph.d.', 'Carol S. Dweck, Ph.d', '2020-07-12 11:32:05', '2020-07-12 17:42:44'),
+(702, 'Sutrino Yahya', 'Sutrino Yahya', '2020-07-12 22:24:07', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `books`
+-- Struktur dari tabel `books`
 --
 
 CREATE TABLE `books` (
   `id` int(10) NOT NULL,
   `title` varchar(40) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `image` varchar(100) DEFAULT NULL,
+  `image` varchar(100) DEFAULT 'uploads\\bookdummy.jpg',
   `genre_id` tinyint(3) NOT NULL,
   `author_id` int(10) NOT NULL,
-  `release_date` date NOT NULL,
+  `release_date` date DEFAULT NULL,
   `status_id` tinyint(3) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`id`, `title`, `description`, `image`, `genre_id`, `author_id`, `release_date`, `status_id`, `created_at`, `updated_at`) VALUES
-(104, 'Sejarah', 'tentang sejarah', 'uploads\\1589729335714_1.png', 1, 1, '2005-02-20', 1, '2020-05-17 14:15:11', '2020-05-17 22:28:55'),
-(106, 'Psikologi Perkembangan Anak Remaja', 'Secara umum, perkembangan merupakan proses pertumbuhan manusia sejak lahir hingga tumbuh dewasa', 'uploads\\1589728326428_2.png', 4, 2, '2020-04-29', 2, '2020-05-17 22:12:05', '2020-05-18 12:44:12'),
-(107, 'Pertempuran Laut Jawa', 'Setelah sukses menghantam Pearl Harbour, Jepang semakin gencar mengadakan ekspansi militernya', 'uploads\\1589728560633_3.png', 5, 3, '2020-04-18', 1, '2020-05-17 22:16:00', '2020-05-18 04:43:41'),
-(108, 'Yasin & Tahlil', 'Tulisan Arab, latin, dan terjemahnya besar dan mudah terbaca.', 'uploads\\1589728713145_4.png', 3, 4, '2020-04-13', 1, '2020-05-17 22:18:32', NULL),
-(109, 'Rumah Hujan', 'Kepada Cakra, lelaki yang mencintai ibunya,', 'uploads\\1589728855084_5.png', 1, 5, '2020-04-13', 2, '2020-05-17 22:20:54', '2020-05-18 13:06:54'),
-(110, 'Seni Mengajarkan Berpuasa pada Anak', 'Seni Mengajarkan Berpuasa pada Anak', 'uploads\\1589729085639_6.png', 3, 5, '2020-04-13', 1, '2020-05-17 22:24:45', NULL),
-(112, 'Kisah Tanah Jawa 2', 'Horror', 'uploads\\2111.jpg', 1, 1, '2020-06-06', 1, '2020-05-21 09:45:01', NULL),
-(113, 'Kisah Tanah Jawa 2', 'Horror', 'uploads\\1590029464582_11.jpg', 1, 1, '2020-06-06', 1, '2020-05-21 09:51:05', NULL),
-(114, 'Kisah Tanah Jawa 2', 'Horror', 'uploads\\1590029646343_11.jpg', 1, 1, '2020-06-06', 1, '2020-05-21 09:54:06', NULL),
-(115, 'Kisah Tanah Jawa 2', 'Horror', 'uploads\\1590030314712_11.jpg', 1, 1, '2020-06-06', 1, '2020-05-21 10:05:15', NULL),
-(116, 'Kisah Tanah Jawa 2', 'Horror', '[object Object]', 1, 1, '2020-06-06', 1, '2020-05-22 14:06:48', NULL),
-(117, 'Kisah Tanah Jawa 2', 'Horror', 'uploads\\1590131255095_11.jpg', 1, 1, '2020-06-06', 1, '2020-05-22 14:07:36', NULL),
-(118, 'Kisah Tanah Jawa 2', 'Horror', 'uploads\\1590131343629_11.jpg', 1, 1, '2020-06-06', 1, '2020-05-22 14:09:05', NULL),
-(119, 'Kisah Tanah Jawa', 'Horror', 'uploads\\1590828141949_10277651_783861248305696_2381725018213295719_n.jpg', 1, 1, '2020-06-06', 3, '2020-05-30 15:42:21', '2020-05-31 08:38:35');
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `book_statuses`
+-- Struktur dari tabel `book_statuses`
 --
 
 CREATE TABLE `book_statuses` (
@@ -101,18 +82,19 @@ CREATE TABLE `book_statuses` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `book_statuses`
+-- Dumping data untuk tabel `book_statuses`
 --
 
 INSERT INTO `book_statuses` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
 (1, 'Available', 'books ready to be borrowed\r\n', '2020-05-16 05:43:13', '2020-05-17 13:19:28'),
 (2, 'Unavailable', 'the book is being borrowed', '2020-05-13 08:44:52', '2020-05-17 13:19:12'),
-(3, 'On Prosess', 'on proses loan for user', '2020-05-17 05:05:04', '2020-05-17 13:18:29');
+(3, 'On Prosess', 'on proses loan for user', '2020-05-17 05:05:04', '2020-05-17 13:18:29'),
+(4, 'Available', 'books ready to be borrowed', '2020-06-14 19:05:53', '2020-06-14 19:06:04');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `genres`
+-- Struktur dari tabel `genres`
 --
 
 CREATE TABLE `genres` (
@@ -123,20 +105,21 @@ CREATE TABLE `genres` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `genres`
+-- Dumping data untuk tabel `genres`
 --
 
 INSERT INTO `genres` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'Art', '2020-05-16 05:34:52', '2020-05-17 13:16:25'),
-(2, 'Technology', '2020-05-15 21:55:59', '2020-05-17 13:16:45'),
-(3, 'Religious', '2020-05-16 05:36:26', '2020-05-17 13:23:25'),
-(4, 'Science', '2020-05-17 22:07:54', NULL),
-(5, 'Sejarah', '2020-05-17 22:14:24', NULL);
+(1, 'Horror', '2020-07-06 07:32:32', NULL),
+(3, 'Geografi', '2020-07-06 07:33:03', NULL),
+(6, 'Romance', '2020-07-06 08:33:21', NULL),
+(26, 'Humor', '2020-07-12 14:56:12', '2020-07-12 16:25:27'),
+(27, 'Psikologi', '2020-07-12 14:57:18', '2020-07-12 16:24:36'),
+(28, 'Indiana', '2020-07-12 18:01:17', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `logs`
+-- Struktur dari tabel `logs`
 --
 
 CREATE TABLE `logs` (
@@ -148,17 +131,23 @@ CREATE TABLE `logs` (
   `status` tinyint(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `logs`
+-- Struktur dari tabel `ratings`
 --
 
-INSERT INTO `logs` (`id`, `time`, `user_email`, `type`, `description`, `status`) VALUES
-(52, '2020-05-31 01:32:30', 'user10@lib.com', 0, 'login', 0);
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `rate` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `roles`
+-- Struktur dari tabel `roles`
 --
 
 CREATE TABLE `roles` (
@@ -169,7 +158,7 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `roles`
+-- Dumping data untuk tabel `roles`
 --
 
 INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
@@ -179,7 +168,7 @@ INSERT INTO `roles` (`id`, `name`, `created_at`, `updated_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transactions`
+-- Struktur dari tabel `transactions`
 --
 
 CREATE TABLE `transactions` (
@@ -193,18 +182,12 @@ CREATE TABLE `transactions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `transactions`
+-- Trigger `transactions`
 --
-
-INSERT INTO `transactions` (`id`, `transaction_date`, `user_id`, `book_id`, `status_id`, `created_at`, `updated_at`) VALUES
-(20, '2020-05-17', 43, 107, 1, '2020-05-17 22:57:39', '2020-05-18 04:43:41'),
-(21, '2020-09-09', 45, 106, 2, '2020-05-18 12:32:15', '2020-05-18 12:44:12'),
-(22, '2020-09-09', 46, 109, 2, '2020-05-18 13:04:55', '2020-05-18 13:06:54'),
-(23, '2020-12-12', 48, 119, 3, '2020-05-31 08:38:35', NULL);
-
---
--- Triggers `transactions`
---
+DELIMITER $$
+CREATE TRIGGER `after_delete_transaction` AFTER DELETE ON `transactions` FOR EACH ROW UPDATE books set books.status_id = 1 WHERE books.id = old.book_id
+$$
+DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER `update_status_book_after_insert` AFTER INSERT ON `transactions` FOR EACH ROW BEGIN
 UPDATE books set books.status_id = new.status_id WHERE books.id = new.book_id;
@@ -221,7 +204,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaction_statuses`
+-- Struktur dari tabel `transaction_statuses`
 --
 
 CREATE TABLE `transaction_statuses` (
@@ -232,18 +215,19 @@ CREATE TABLE `transaction_statuses` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `transaction_statuses`
+-- Dumping data untuk tabel `transaction_statuses`
 --
 
 INSERT INTO `transaction_statuses` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Return the Book', '2020-05-13 08:46:39', '2020-05-18 12:45:12'),
 (2, 'Borrowed', '2020-05-13 08:46:39', '2020-05-18 12:45:16'),
-(3, 'Pending', '2020-05-18 12:39:49', NULL);
+(3, 'Pending', '2020-05-18 12:39:49', NULL),
+(4, 'Cancel', '2020-06-14 19:10:36', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -256,21 +240,24 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Trigger `users`
 --
-
-INSERT INTO `users` (`id`, `email`, `password`, `role_id`, `created_at`, `updated_at`) VALUES
-(34, 'admin@lib.com', '$2b$10$3sHeIcL8Bj.aBrxPpjnU/e1HOXTwBSrHxIo1g9fR59jFhK.qsF2ju', 1, '2020-05-15 20:30:23', '2020-05-15 20:34:06'),
-(43, 'user2@lib.com', '$2b$10$uavZRd6oguegLc0aLanQR.NYz3fpmNa3Fok/Tu482pPP3yT6McHEe', 2, '2020-05-17 14:26:39', NULL),
-(44, 'user@lib.com', '$2b$10$XiRBwKdfV.jgpv1xQ8ajlO2hrJFDF.20riOLYyiiyIMZJ.9ZuQAna', 2, '2020-05-17 22:00:58', NULL),
-(45, 'user3@lib.com', '$2b$10$E22lIwEHHvy1sskE7qax1OhKn6hKDmXD56otm71EkGToNzE6eNjS6', 2, '2020-05-18 12:29:23', NULL),
-(46, 'user5@lib.com', '$2b$10$nhIRAAbCFTPNKtsIIf/DyekYkyPZjGr5YWl.cfRLh5wEDrnBFZ/4K', 2, '2020-05-18 13:02:09', NULL),
-(47, 'user6@lib.com', '$2b$10$MKV3Dc/98b6IPmPbG3T2fOMuhVmlGS8fncoeS5qpBHhEg2YIQ/G4G', 2, '2020-05-28 10:35:29', NULL),
-(48, 'user10@lib.com', '$2b$10$H0jiTdFYxSGSWg9n4IfIcuh/Dd14dSjFRPpwVPiKQuanoYg/i484u', 2, '2020-05-28 10:41:06', NULL);
-
---
--- Triggers `users`
---
+DELIMITER $$
+CREATE TRIGGER `after_add_users` AFTER INSERT ON `users` FOR EACH ROW INSERT INTO `user_details` (
+    `user_id`,
+    `picture`,
+    `name`,
+    `birthdate`,
+    `gender`) 
+VALUES (
+     NEW.id,
+     'uploads/default.png', 	 
+    'unknown',
+    '2020-05-05',
+     'Male'
+)
+$$
+DELIMITER ;
 DELIMITER $$
 CREATE TRIGGER `delete_user_on_detail` AFTER DELETE ON `users` FOR EACH ROW BEGIN
 DELETE FROM user_details
@@ -282,44 +269,32 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_details`
+-- Struktur dari tabel `user_details`
 --
 
 CREATE TABLE `user_details` (
   `id` int(10) NOT NULL,
   `user_id` int(10) DEFAULT NULL,
-  `name` varchar(25) NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
   `picture` varchar(100) DEFAULT NULL,
-  `birthdate` date NOT NULL,
-  `gender` enum('Male','Female') NOT NULL,
+  `birthdate` date DEFAULT NULL,
+  `gender` enum('Male','Female') DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user_details`
---
-
-INSERT INTO `user_details` (`id`, `user_id`, `name`, `picture`, `birthdate`, `gender`, `created_at`, `updated_at`) VALUES
-(60, 34, 'user', 'uploads\\1589630700378_1403075_651107551589632_1552316277_o.jpg', '2020-06-06', 'Male', '2020-05-16 19:04:59', NULL),
-(76, 43, 'kamal', NULL, '2020-06-06', 'Male', '2020-05-17 14:59:20', '2020-05-17 22:40:52'),
-(77, 45, 'user', 'uploads\\1589779869279_download.jpg', '2020-06-06', 'Male', '2020-05-18 12:31:09', NULL),
-(78, 46, 'andi', 'uploads\\1589781801604_download.jpg', '2020-06-06', 'Male', '2020-05-18 13:03:21', NULL),
-(79, 47, 'kamal', 'uploads\\1590637011558_1.png', '2020-06-06', 'Male', '2020-05-28 10:36:50', NULL),
-(80, 48, 'kamsong', 'uploads\\1590637615006_1.png', '2020-06-06', 'Male', '2020-05-28 10:46:53', NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `authors`
+-- Indeks untuk tabel `authors`
 --
 ALTER TABLE `authors`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `books`
+-- Indeks untuk tabel `books`
 --
 ALTER TABLE `books`
   ADD PRIMARY KEY (`id`),
@@ -329,31 +304,37 @@ ALTER TABLE `books`
   ADD KEY `status_id` (`status_id`);
 
 --
--- Indexes for table `book_statuses`
+-- Indeks untuk tabel `book_statuses`
 --
 ALTER TABLE `book_statuses`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `genres`
+-- Indeks untuk tabel `genres`
 --
 ALTER TABLE `genres`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `logs`
+-- Indeks untuk tabel `logs`
 --
 ALTER TABLE `logs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `roles`
+-- Indeks untuk tabel `ratings`
+--
+ALTER TABLE `ratings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `transactions`
+-- Indeks untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
   ADD PRIMARY KEY (`id`),
@@ -362,92 +343,98 @@ ALTER TABLE `transactions`
   ADD KEY `status_id` (`status_id`);
 
 --
--- Indexes for table `transaction_statuses`
+-- Indeks untuk tabel `transaction_statuses`
 --
 ALTER TABLE `transaction_statuses`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD KEY `role_id` (`role_id`);
 
 --
--- Indexes for table `user_details`
+-- Indeks untuk tabel `user_details`
 --
 ALTER TABLE `user_details`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `user_id` (`user_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `authors`
+-- AUTO_INCREMENT untuk tabel `authors`
 --
 ALTER TABLE `authors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=704;
 
 --
--- AUTO_INCREMENT for table `books`
+-- AUTO_INCREMENT untuk tabel `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `book_statuses`
+-- AUTO_INCREMENT untuk tabel `book_statuses`
 --
 ALTER TABLE `book_statuses`
   MODIFY `id` tinyint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `genres`
+-- AUTO_INCREMENT untuk tabel `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `id` tinyint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` tinyint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
--- AUTO_INCREMENT for table `logs`
+-- AUTO_INCREMENT untuk tabel `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `roles`
+-- AUTO_INCREMENT untuk tabel `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id` tinyint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `transactions`
+-- AUTO_INCREMENT untuk tabel `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `transaction_statuses`
+-- AUTO_INCREMENT untuk tabel `transaction_statuses`
 --
 ALTER TABLE `transaction_statuses`
-  MODIFY `id` tinyint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` tinyint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=49;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `user_details`
+-- AUTO_INCREMENT untuk tabel `user_details`
 --
 ALTER TABLE `user_details`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 DELIMITER $$
 --
--- Events
+-- Event
 --
 CREATE DEFINER=`root`@`%` EVENT `delete_logs_login` ON SCHEDULE EVERY 5 MINUTE STARTS '2020-05-30 22:39:16' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM logs WHERE type = 0 AND time < DATE_SUB( CURRENT_TIME(), INTERVAL 50 MINUTE)$$
 
